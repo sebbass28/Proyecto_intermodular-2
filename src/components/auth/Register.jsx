569f60955abd9c2c14a4f882a22d47c2365108ac
Juan Sebastián Agudelo Castrillon <juaagucas@alu.edu.gva.es>
2025-10-25 16:08:26 +0000

    quitar fondo blanco

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -18,7 +18,7 @@ function Register() {
 
   return (
     <div className="flex items-center justify-center p-12">
-      <div className="mx-auto w-full max-w-[550px] bg-white">
+      <div className="mx-auto w-full max-w-[550px]">
         <form>
           <div className="mb-5">
             <label
