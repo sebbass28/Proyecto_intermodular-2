31d2a5195891aa6c9dcca786e302b55185b8180b
Juan Sebastián Agudelo Castrillon <juaagucas@alu.edu.gva.es>
2025-10-25 14:10:16 +0000

    se eliminó  app en main.jsx duplicado

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
-    <App />
   </React.StrictMode>
 );
