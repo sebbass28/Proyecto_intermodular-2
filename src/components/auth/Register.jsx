3151656cdd06bce06812e84ee1fa12c677426d4c
Juan Sebastián Agudelo Castrillon <juaagucas@alu.edu.gva.es>
2025-10-25 14:15:25 +0000

    arreglar componente Register.jsx

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useAuthStore } from "../../store/authStore";
 import { useEffect } from "react";
 
-function App() {
+function Register() {
   const [count, setCount] = useState(0);
   const {
     userName,
@@ -14,18 +14,16 @@ function App() {
     birthDay,
     address,
   } = useAuthStore();
-  useEffect(() => {
-    tryAutoLogin();
-  }, []);
+  useEffect(() => {}, []);
 
   return (
-    <div class="flex items-center justify-center p-12">
-      <div class="mx-auto w-full max-w-[550px] bg-white">
+    <div classNameName="flex items-center justify-center p-12">
+      <div classNameName="mx-auto w-full max-w-[550px] bg-white">
         <form>
-          <div class="mb-5">
+          <div classNameName="mb-5">
             <label
               for="name"
-              class="mb-3 block text-base font-medium text-[#07074D]"
+              classNameName="mb-3 block text-base font-medium text-[#07074D]"
             >
               Nombre Completo
             </label>
@@ -34,13 +32,13 @@ function App() {
               name="name"
               id="name"
               placeholder="Full Name"
-              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              classNameName="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
             />
           </div>
-          <div class="mb-5">
+          <div classNameName="mb-5">
             <label
               for="phone"
-              class="mb-3 block text-base font-medium text-[#07074D]"
+              className="mb-3 block text-base font-medium text-[#07074D]"
             >
               Telefono
             </label>
@@ -49,13 +47,13 @@ function App() {
               name="phone"
               id="phone"
               placeholder="Enter your phone number"
-              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
             />
           </div>
-          <div class="mb-5">
+          <div className="mb-5">
             <label
               for="email"
-              class="mb-3 block text-base font-medium text-[#07074D]"
+              className="mb-3 block text-base font-medium text-[#07074D]"
             >
               Correo
             </label>
@@ -64,15 +62,15 @@ function App() {
               name="email"
               id="email"
               placeholder="Enter your email"
-              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
             />
           </div>
-          <div class="-mx-3 flex flex-wrap">
-            <div class="w-full px-3 sm:w-1/2">
-              <div class="mb-5">
+          <div className="-mx-3 flex flex-wrap">
+            <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
                 <label
                   for="date"
-                  class="mb-3 block text-base font-medium text-[#07074D]"
+                  className="mb-3 block text-base font-medium text-[#07074D]"
                 >
                   Fecha
                 </label>
@@ -80,58 +78,58 @@ function App() {
                   type="date"
                   name="date"
                   id="date"
-                  class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  classNameName="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                 />
               </div>
             </div>
           </div>
 
-          <div class="mb-5 pt-3">
-            <label class="mb-5 block text-base font-semibold text-[#07074D] sm:text-xl">
+          <div classNameName="mb-5 pt-3">
+            <label className="mb-5 block text-base font-semibold text-[#07074D] sm:text-xl">
               Dirección
             </label>
-            <div class="-mx-3 flex flex-wrap">
-              <div class="w-full px-3 sm:w-1/2">
-                <div class="mb-5">
+            <div className="-mx-3 flex flex-wrap">
+              <div className="w-full px-3 sm:w-1/2">
+                <div className="mb-5">
                   <input
                     type="text"
                     name="area"
                     id="area"
                     placeholder="Enter area"
-                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
               </div>
-              <div class="w-full px-3 sm:w-1/2">
-                <div class="mb-5">
+              <div className="w-full px-3 sm:w-1/2">
+                <div className="mb-5">
                   <input
                     type="text"
                     name="city"
                     id="city"
                     placeholder="Enter city"
-                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
               </div>
-              <div class="w-full px-3 sm:w-1/2">
-                <div class="mb-5">
+              <div className="w-full px-3 sm:w-1/2">
+                <div className="mb-5">
                   <input
                     type="text"
                     name="state"
                     id="state"
                     placeholder="Enter state"
-                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
               </div>
-              <div class="w-full px-3 sm:w-1/2">
-                <div class="mb-5">
+              <div className="w-full px-3 sm:w-1/2">
+                <div className="mb-5">
                   <input
                     type="text"
                     name="post-code"
                     id="post-code"
                     placeholder="Post Code"
-                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
               </div>
@@ -139,7 +137,7 @@ function App() {
           </div>
 
           <div>
-            <button class="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none">
+            <button className="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none">
               Registrarse
             </button>
           </div>
@@ -149,4 +147,4 @@ function App() {
   );
 }
 
-export default App;
+export default Register;
