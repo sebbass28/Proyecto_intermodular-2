b0a7d78a0c97c287e1fdfe98e7e6e8be804002cf
Juan Sebastián Agudelo Castrillon <juaagucas@alu.edu.gva.es>
2025-10-25 14:06:10 +0000

    se han añadido e instalado la dependencia de react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./components/auth/Register.jsx";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
