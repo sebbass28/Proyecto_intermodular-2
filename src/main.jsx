663fb7e5c9eba2585f67c59a9bc6c600847dd1e0
sebbass28 <juaagucas@alu.edu.gva.es>
2025-12-07 17:05:09 +0100

    barra de busqueda, perfil y configuraciones

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/auth/Register.jsx";
 import ForgotPassword from "./pages/auth/ForgotPassword.jsx";
-import DashboardHome from "./components/dashboard/DashboardHome.jsx";
 import Login from "./pages/auth/Login.jsx";
 import "./index.css";
+import DashboardHome from "./components/dashboard/DashboardHome.jsx";
+import SettingsView from "./components/dashboard/SettingsView.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/register" element={<Register />} />
         <Route path="/forgotPassword" element={<ForgotPassword />} />
         <Route path="/dashboard" element={<DashboardHome />} />
+        <Route path="/settings" element={<SettingsView />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
